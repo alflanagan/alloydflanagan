6edf2ad7a62456c09215277509f03ea998c5cf22
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2019-09-16 16:00:20 -0400

    wire up index views for blog, portfolio

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse(content=b"<p>Congrats, you've reached the blogs index view</p>")
