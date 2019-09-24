1d80ddcb6523ca3688990eb11b43ecfc9e0ffeaf
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2019-09-24 16:36:05 -0400

    Add page footer

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,15 @@
 """Views which display (relatively) static pages"""
 from django.shortcuts import render
+from django import get_version
 
 def about_me(request):
     """Simple About Me page"""
-    return render(request, "pages/about_me.html")
+    return render(request, "pages/about_me.html", context={"django_version": get_version()})
 
 def home(request):
     """Initial view for users"""
-    return render(request, "pages/home.html")
+    return render(request, "pages/home.html", context={"django_version": get_version()})
 
 def contact(request):
     """My contact information page"""
-    return render(request, "pages/contact.html")
+    return render(request, "pages/contact.html", context={"django_version": get_version()})
