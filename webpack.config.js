7aa2fb9b5628b8118d18cd297099e3b1c2e93f52
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2019-09-24 14:42:11 -0400

    :bug: fix problem with background image

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,10 @@ module.exports = {
       }, {
         test: /\.(png|jpe?g|gif)$/i,
         use: [{
-          loader: 'file-loader'
+          loader: 'file-loader',
+          options: {
+            outputPath: 'images'
+          }
         }]
       }
     ]
