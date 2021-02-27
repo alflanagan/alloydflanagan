abd166d90c0d5ce979191566222d710a615e212f
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2021-02-27 10:48:35 -0500

    fix manage command

--- startup.sh
+++ startup.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 manage() {
-  pipenv run ./manage.py "$@"
+  pipenv run python ./manage.py "$@"
 }
 
 manage migrate
-# manage collectstatic --noinput
 manage runserver 0.0.0.0:3000
